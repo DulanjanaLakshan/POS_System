2f084e911fb9482572f0499c20c10b209ce941cf
dulanjanalakshan <dulanjana20013@gmail.com>
2022-02-28 14:10:12 +0530

    crate item cud using mvc

--- assets/js/itemController.js
+++ assets/js/itemController.js
@@ -2,7 +2,70 @@
 * @author : U.K.Dulanjana Lakshan Kumarasingha
 * @since : 0.1.0
 */
-/*----------------------- Item CRUD ------------------------*/
-$("#btnItemSaveOrUpdate").click(function () {
+let regItemCode = /^(I00-)[0-9]{4}$/;
+let regItemName = /^[A-z 0-9.]{3,}$/;
+let regItemUnitPrice = /^[0-9]{1,}([.][0-9]{2})?$/;
+let regItemQty = /^[0-9]{1,}$/;
 
-});
+let searchItemCode;
+
+$("#txtItemID,#txtItemName,#txtItemDescription,#txtItemQTY,#txtItemPrice").on('keydown', function (event) {
+    if (event.key == "Tab") {
+        event.preventDefault();
+    }
+})
+$("#txtItemID,#txtItemName,#txtItemDescription,#txtItemQTY,#txtItemPrice").on('blur', function () {
+    itemValidation();
+})
+
+function itemValidation() {
+
+}
+
+$("#btnSaveItem").click(function () {
+    addItem();
+    loadAllItem();
+})
+
+function addItem() {
+    let itemId = $("#txtItemID").val();
+    let itemName = $("#txtItemName").val();
+    let itemDescription = $("#txtItemDescription").val();
+    let itemQTY = $("#txtItemQTY").val();
+    let itemPrice = $("#txtItemPrice").val();
+
+    var items = new ItemDTO(itemId, itemName, itemDescription, itemQTY, itemPrice);
+    itemDB.push(items);
+}
+
+function loadAllItem() {
+    $("#ItemTable").empty();
+    for (var i = 0; i < itemDB.length; i++) {
+        let row = `<tr><td>${itemDB[i].getCode()}</td><td>${itemDB[i].getName()}</td><td>${itemDB[i].getDescription()}</td><td>${itemDB[i].getQTY()}</td><td>${itemDB[i].getPrice()}</td></tr>`;
+        $("#ItemTable").append(row);
+    }
+}
+
+function deleteItem() {
+    let searchItem = $("#txtItemID").val();
+    for (var i = 0; i < itemDB.length; i++) {
+        if (itemDB[i].getCode() == searchItem) {
+            itemDB.splice(i, 1);
+            clearDeleteItemFields();
+        }
+    }
+}
+
+function clearDeleteItemFields() {
+    $("#srcItemID").focus();
+    $("#txtItemID").val("");
+    $("#txtItemName").val("");
+    $("#txtItemDescription").val("");
+    $("#txtItemQTY").val("");
+    $("#txtItemPrice").val("");
+}
+
+$("#btnItemDelete").click(function () {
+    deleteItem();
+    loadAllItem();
+})
---
+++ model/itemDTO.js
@@ -0,0 +1,42 @@
+/*
+* @author : U.K.Dulanjana Lakshan Kumarasingha
+* @since : 0.1.0
+*/
+function ItemDTO(code, name, description, qty, price) {
+    var I_code = code;
+    var I_name = name;
+    var I_description = description;
+    var I_qty = qty;
+    var I_price = price;
+
+    this.getCode = function () {
+        return I_code;
+    }
+    this.getName = function () {
+        return I_name;
+    }
+    this.getDescription = function () {
+        return I_description;
+    }
+    this.getQTY = function () {
+        return I_qty;
+    }
+    this.getPrice = function () {
+        return I_description;
+    }
+    this.setCode = function () {
+        I_code = code;
+    }
+    this.setName = function () {
+        I_name = name;
+    }
+    this.setDescription = function () {
+        I_description = description;
+    }
+    this.setQTY = function () {
+        I_qty = qty;
+    }
+    this.setPrice = function () {
+        I_price = price;
+    }
+}
